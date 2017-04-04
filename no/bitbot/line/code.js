06e8245bb15c0da1512e47fe777a0f8a2d35eb3c
Sten Roger Sandvik <srs@enonic.com>
2017-04-05 00:12:09 +0200

    Fixed line task

--- no/bitbot/line/code.js
+++ no/bitbot/line/code.js
@@ -1,11 +1,11 @@
 basic.forever(() => {
-    if (bitbot.readLine(bitbot.LineSensor.Right) == 1) {
-        bitbot.motor(bitbot.Motor.Left, 1023)
-        bitbot.motor(bitbot.Motor.Right, -1023)
-    } else if (bitbot.readLine(bitbot.LineSensor.Left) == 1) {
-        bitbot.motor(bitbot.Motor.Right, 1023)
-        bitbot.motor(bitbot.Motor.Left, -1023)
+    if (bitbot.readLine(BBLineSensor.Right) == 1) {
+        bitbot.motor(BBMotor.Left, 1023)
+        bitbot.motor(BBMotor.Right, -1023)
+    } else if (bitbot.readLine(BBLineSensor.Left) == 1) {
+        bitbot.motor(BBMotor.Right, 1023)
+        bitbot.motor(BBMotor.Left, -1023)
     } else {
-        bitbot.motor(bitbot.Motor.All, 1023)
+        bitbot.motor(BBMotor.All, 1023)
     }
 })
